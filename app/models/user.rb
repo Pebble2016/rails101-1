7e9f9e1e04fcb548d6a8b568f9f48959f96e3b59
ypj <ypj2005@126.com>
2017-02-24 00:07:57 +0800

    join or quit group

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,15 @@ class User < ApplicationRecord
   has_many :participated_groups, :through => :group_relationships, :source => :group
 
   def is_member_of?(group)
-    participated_groups.include?(group) 
+    participated_groups.include?(group)
   end
+
+  def join!(group)
+    participated_groups << group
+  end
+
+  def quit!(group)
+    participated_groups.delete(group)
+  end
+
 end
